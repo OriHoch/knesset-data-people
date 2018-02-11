11d89138403e2604749de6cbfe01cda7c451de1c
Ori Hoch <ori@uumpa.com>
2018-02-11 20:18:06 +0200

    minor fix pipelines script

--- pipelines_script.sh
+++ pipelines_script.sh
@@ -4,12 +4,12 @@ RUN_PIPELINE_CMD="${RUN_PIPELINE_CMD:-dpp run}"
 
 RES=0;
 
-! $RUN_PIPELINE_CMD ./download/committees && RES=1;
-! $RUN_PIPELINE_CMD ./download/members && RES=1;
-! $RUN_PIPELINE_CMD ./join-committee-meetings && RES=1;
-! $RUN_PIPELINE_CMD ./join-members && RES=1;
-! $RUN_PIPELINE_CMD ./committee-meeting-attendees && RES=1;
-! $RUN_PIPELINE_CMD ./join-committee-meeting-attendees-mks && RES=1;
-! $RUN_PIPELINE_CMD ./committee-meeting-attendees-mks-stats && RES=1;
+! $RUN_PIPELINE_CMD ./download/committees && RES=1
+! $RUN_PIPELINE_CMD ./download/members && RES=1
+! $RUN_PIPELINE_CMD ./join-committee-meetings && RES=1
+! $RUN_PIPELINE_CMD ./join-members && RES=1
+! $RUN_PIPELINE_CMD ./committee-meeting-attendees && RES=1
+! $RUN_PIPELINE_CMD ./join-committee-meeting-attendees-mks && RES=1
+! $RUN_PIPELINE_CMD ./committee-meeting-attendees-mks-stats && RES=1
 
 exit $RES
